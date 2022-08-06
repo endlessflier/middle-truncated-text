a314e7d12c3482f5c80e9af8631b9f4ea45ff742
Web development <webdeveloper2009@outlook.com>
2022-08-06 01:21:50 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1 className="textbox">Truncated Text</h1>
+      <div className="container">
+        <div className="textbox truncated">
+          Start editing to see some magic happen!
+        </div>
+        <div className="textbox tails">trail text ab</div>
+      </div>
+    </div>
+  );
+}
